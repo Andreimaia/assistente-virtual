345fc2dded52d31e0d090f98dbe71d47609a618b
Andrei <102187905+Andreimaia@users.noreply.github.com>
2022-10-24 18:24:17 -0300

    Add files via upload

---
+++ projeto assistente/app.py
@@ -0,0 +1,56 @@
+from distutils.util import execute
+#import speech_recognition as sr
+from vosk import Model, KaldiRecognizer
+import pyttsx3
+import datetime
+import wikipedia
+import pywhatkit
+#criar um reconhecedor
+model=Model(r'D:\Users\Andrei\Desktop\projeto assistente\Model')
+recognizer=KaldiRecognizer(model,16000)
+audio=KaldiRecognizer.Recognizer()
+#audio=sr.Recognizer()
+maquina= pyttsx3.init()
+maquina.say('bom dia ,senhor')
+maquina.say('como posso lhe ajudar')
+maquina.runAndWait()
+
+def excuta_comando():
+    try:
+        #abri meicrofone para captura
+        with KaldiRecognizer.Microphone() as source:
+       # with sr.Microphone() as source:
+            print('Ouvindo...')
+            voz = audio.listen(source)
+            comando=audio.recognize_google(voz, language='pt-BR')
+            comando=comando.lower()
+        
+            if'sexta feira' in comando:       
+               comando=comando.replace('sexta feira','')
+               maquina.say(comando )
+               maquina.runAndWait()
+    except:
+         print('microfone não esta ok')   
+
+    return comando 
+
+def comando_voz_usuario():
+       comando =excuta_comando()
+       if 'horas' in comando:
+           hora=datetime.datetime.now().strftime('%H:%M')
+           maquina.say('Agora são'+hora)
+           maquina.runAndWait()
+       elif 'procure por' in comando:
+           procurar=comando.replace('procure por','')
+           wikipedia.set_lang('pt')
+           resultado=wikipedia.summary(procurar,2)
+           print(resultado)
+           maquina.say(resultado)
+           maquina.runAndWait()
+       elif 'toque'in comando:
+            musica= comando.replace('toque','')
+            resultado = pywhatkit.playonyt(musica)
+            maquina.say('tocando musica')
+            maquina.runAndWait()    
+
+comando_voz_usuario()
